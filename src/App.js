f67d2c3be2924c9b5d6ac2e78b243d118503e1c2
”MahmoudAlZayed” <mz.mahmoudal@gmail.com>
2025-02-19 16:51:12 +0100

    hdinig api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useEffect, useState } from "react";
+import Axios from "axios";
+import "./App.css";
 
 function App() {
+  const myRef = useRef(null);
+  const [search, setSearch] = useState("");
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    Axios.get(
+      `http://localhost:8000/books`  
+    ).then((res) => {
+      console.log(res.data);
+      setBooks(res.data);
+    });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input
+        className="searchBar"
+        value={search}
+        onChange={(e) => {
+          setSearch(e.target.value);
+        }}
+      />
+      <div className="booksListContainer">
+        {books
+          .filter((book) => {
+            return search.toLowerCase() === ""
+              ? book
+              : book.title.toLowerCase().includes(search);
+          })
+          .map((book, id) => (
+            <div key={id} className="booksList" ref={myRef}>
+              <img
+                src={book.image_url}
+                alt={book.title + "book cover image"}
+                className="booksCover"
+              />
+              <h1> {book.title} </h1>
+
+              <h2>{book.year}</h2>
+              <p>{book.description}</p>
+            </div>
+          ))}
+      </div>
+       
+    </>
   );
 }
 
